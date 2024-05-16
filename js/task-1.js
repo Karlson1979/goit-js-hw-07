a058f2c1695fd722f16d3bc208c2c1e951c633a3
Tochka <tochka114@gmail.com>
2024-05-16 22:53:56 +0300

    task-1

---
+++ js/task-1.js
@@ -0,0 +1,22 @@
+
+
+
+const categoriesList = document.querySelector('#categories');
+
+
+const categoryItems = document.querySelectorAll('.item');
+
+
+console.log('Number of categories:', categoryItems.length);
+
+
+categoryItems.forEach(item => {
+ 
+  const title = item.querySelector('h2').textContent;
+
+  
+  const elementsCount = item.querySelectorAll('li').length;
+
+  
+  console.log(`Category: ${title}\nElements: ${elementsCount}`);
+});
