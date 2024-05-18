0459e4eb0badbb6a8e6a0862f50784d022725410
Tochka <tochka114@gmail.com>
2024-05-18 17:12:54 +0300

    task-4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,12 @@
+const formEl = document.querySelector(".login-form");
 
+formEl.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const email = formEl.elements.email.value.trim();
+  const password = formEl.elements.password.value.trim();
+  if (email === "" || password === "") {
+    alert("All form fields must be filled in");
+  }
+  const data = { email, password };
+  console.log(data);
+});
